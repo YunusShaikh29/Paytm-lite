21f822805702c1a221aedf49a2c552bc2a068bf5
Yunus Shaikh <yunusshaikh29082@gmail.com>
2024-02-15 22:52:13 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const rootRouter = require('./routes/index')
+require("dotenv").config()
 // const cors = require("cors")
+const PORT = process.env.PORT
 const app = express()
 
 // app.use(cors())
-const PORT = 7070
 
 
 app.use(express.json())
